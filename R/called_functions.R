e9a43f127bc6c9d948b96729b5e4a3819b6bffba
Jonathan Cornelissen <Jonathancornelissen@hotmail.com>
2014-03-28 00:48:16 -0300

    complete rewrite of the functions previously using regex, now with R parse tree

--- R/called_functions.R
+++ R/called_functions.R
@@ -1,7 +1,7 @@
-called_functions <- function( user.code=DM.user.code){
-  parseData <- getParseData(parse(text=DM.user.code))
-  id <- with(parseData, id[token == "SYMBOL_FUNCTION_CALL"])
-  call_funs <- sapply(id, function(x) getParseText(parseData, id=x))
+called_functions <- function( user_code=DM.user.code){
+  parseData <- getParseData(parse(text=user_code));
+  id <- with(parseData, id[token == "SYMBOL_FUNCTION_CALL"]);
+  call_funs <- sapply(id, function(x) getParseText(parseData, id=x));
   
-  return(call_funs)  
-}
+  return(call_funs);
+} 
--- R/function_has_arguments.R
+++ R/function_has_arguments.R
@@ -1,51 +1,84 @@
-student_used_function = function(fun){
-  # How many times was the function called?  
-  fun_called = sum(called_functions() %in% fun);
-  return(fun_called)
+expressions_for_function = function(fun = NULL, code=DM.user.code){
+  # Parse user code and get parse information: 
+  parseData <- getParseData(parse(text=code)); 
+  ids_of_expressions = with(parseData, parent[text == fun & token == "SYMBOL_FUNCTION_CALL"]); 
+  ids_of_expressions = parseData$parent[ parseData$id %in% ids_of_expressions ]; 
+  called_expressions <- sapply(ids_of_expressions, function(x) getParseText(parseData, id=x))
+  
+  return(called_expressions)
 }
 
-check_arguments = function(student_arguments, args, values,eval){
-  relevant_student_args = student_arguments[ names(student_arguments) %in% args ];
-  if(length(relevant_student_args)==0){ return(FALSE) }
-  match_vector = c();
-  for(i in 1:length(relevant_student_args)){
-    index = which( args == names(relevant_student_args)[i]);
-    if(length(index)==0){ 
-      return(FALSE) 
-    } else{
-      if (eval[index]==FALSE){
-        match_vector[i] =  try(identical(as.character(relevant_student_args[index]), as.character(values[index])));
-      } else {
-        match_vector[i] =  try(identical(eval(parse(text=relevant_student_args[index])), eval(parse(text=values[index]))));        
-      }
-    }                      
-  }
-  return(all(match_vector)); # All values should be correct!
+arguments_for_expression = function(expression=NULL,fun=NULL){
+  arguments <- match.call(get(fun), call=parse(text=expression))[-1];
+  arguments_list = as.list(arguments);
+  
+  # wtf, symbols? why oh why R.
+  # start ugly-fix:
+  names = names(arguments_list);
+  selection = sapply(arguments_list,is.symbol)
+  arguments_list[selection] = as.character(arguments_list[selection]);
+  names(arguments_list) = names;
+  # end ugly-fix
+  
+  return(arguments_list)
 }
+
+check_function_arguments = function(student_arguments=NULL,args=args,values=values,eval=eval){
+  relevant_student_args = student_arguments[ names(student_arguments) %in% args ];
+  if(length(relevant_student_args)!=length(args)){ return(FALSE) }
+  
+  match_vector = c();
+  # Situation with values:
+  if(!is.null(values)){
+    # Loop over relevant arguments: 
+    for(i in 1:length(args)){
+      index = which( names(relevant_student_args) %in% args[i] );
+      if(length(index)==0){ 
+        return(FALSE) 
+      } else{
+        if (eval[i]==FALSE){
+          match_vector[i] =  try(all.equal(as.character(relevant_student_args[[index]]), as.character(values[i])));
+        } else {
+          match_vector[i] =  try(all.equal(eval(parse(text=relevant_student_args[[index]])), eval(parse(text=values[i]))));        
+        }
+      }                      
+    }
+    match_vector = as.logical(match_vector);
+    if(any(is.na(match_vector))){ match_vector=FALSE }
+    return(all(match_vector)); # All values should be correct!
+  } else if (is.null(values)){
+    # Situation without values: 
+    # Just check whether all args were called by student:
+    return(all(args %in%  names(student_arguments)))    
+  }  
+}
+
 
 function_has_arguments = function(fun=NULL,args=NULL,values=NULL,eval=NULL){
-  if (is.null(eval)){ eval = rep(FALSE,length(args)) }
-  if (!is.null(values) && length(eval)!=length(values)){ stop("eval vector should have same lenght as values vector, for obvious reasons ;-).") }
-    
-  if ((!is.null(fun)) && student_used_function(fun)!=0){
-    arguments = get_arguments(fun);   
-    
-    if (!is.null(args) && is.null(values)){ 
-      # Just check whether these arguments were provided, irrespective of their value
-      has_arguments = c();
-      for (i in 1:length(arguments)){ # Loop over different times a function is used
-        has_arguments[i] = all(args %in% names(arguments[[i]]));
-      }
-      return(sum(has_arguments))
-    } else if (!is.null(args) && !is.null(values)){
-      if(length(args)!=length(values)){ stop("you idiot, give args and values the same length.")}
-      # Check whether the correct value was assign to the correct argument by a student
-      correct_arguments = c();
-      for (i in 1:length(arguments)){ # Loop over different times a function is used
-        correct_arguments[i] = check_arguments(student_arguments=arguments[[i]], args,values,eval);
+  if (is.null(eval)){ 
+    eval = rep(FALSE,length(args)) 
+  }
+  if (!is.null(values) && length(eval)!=length(values)){ 
+    stop("eval vector should have same lenght as values vector, for obvious reasons ;-).") 
+  }
+  # Step 1: Get the expressions in which the function is used:
+  expressions = expressions_for_function(fun);
+  if(length(expressions)==0){ return(FALSE) }
+  
+  # Step 2: Get the arguments
+  argument_list = list();
+  for(i in 1:length(expressions)){
+    argument_list[[i]] = arguments_for_expression(expressions[i],fun=fun)
+  }
+  
+  # Step 3: Find matches:
+  if (!is.null(args) && !is.null(values)){
+    matches = c();
+    for(i in 1:length(argument_list)){
+      matches[i] = check_function_arguments(argument_list[[i]],args,values,eval);
     }
-    return(sum(correct_arguments))
+    return(sum(matches))
   }
-  }
-  return(FALSE)
 }
+
+# TODO: TEST THIS! specifically for special functions e.g. mean
